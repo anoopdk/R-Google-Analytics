8f4c7e0453affb9ea2f649610e4a6394a11a1666
Anoop DK <anoopdk@gmail.com>
2019-03-25 18:28:18 +0530

    Add files via upload

---
+++ DataViz_GoogleAnalytics.R
@@ -0,0 +1,66 @@
+install.packages("googleAuthR")
+install.packages("googleAnalyticsR")
+install.packages("ggplot2")
+
+# load libraries
+library("googleAuthR")
+library("googleAnalyticsR")
+library("ggplot2")
+
+# authorize connection with Google Analytics servers
+ga_auth()
+
+gadata <- google_analytics(viewId = 1234567,     # view ID changed
+                            date_range = c(
+                              today() - 30, 	# start date
+                              today()		# end date
+                            ), 
+                           metrics = c("sessions"),
+                           dimensions = c("date"),
+                           max = 6000)
+
+# line chart
+ggplot(gadata,aes(x=date,y=sessions,group=1)) +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) # some styles to pivot x-axis labels
+
+
+# scatter plot
+ggplot(gadata, aes(x=date, y=sessions)) +
+  geom_point()
+
+ggplot(gadata, aes(x=date, y=sessions)) +
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) # styling x-axis
+
+# scatter plot - point size
+ggplot(gadata, aes(x=date, y=sessions, size = sessions)) +
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# scatter plot - point color
+ggplot(gadata, aes(x=date, y=sessions, size = sessions, color = sessions)) +
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# scatter plot with trend line
+ggplot(data = gadata, aes(x = gadata$date,y = gadata$sessions) ) + 
+  geom_point() + 
+  geom_smooth() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# box plot
+gadata2 <- google_analytics(viewId = 1234567,     # view ID changed
+                            date_range = c(
+                              today() - 30, 	# start date
+                              today()		# end date
+                            ), 
+                            metrics = c("sessions","avgSessionDuration"),
+                            dimensions = c("dayOfWeek","date"),
+                            max = 5000)
+
+ggplot(data = gadata2, aes(x = dayOfWeek, y = sessions)) + 
+  geom_boxplot()
+
+ggplot(data = gadata2, aes(x = dayOfWeek, y = avgSessionDuration)) + 
+  geom_boxplot()
