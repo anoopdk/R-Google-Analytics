8ba34cc39adb8db72af2e31617571ff0d91e7a5e
Anoop DK <anoopdk@gmail.com>
2019-03-27 11:21:31 +0530

    Update DataViz_GoogleAnalytics.R

--- DataViz_GoogleAnalytics.R
+++ DataViz_GoogleAnalytics.R
@@ -17,7 +17,7 @@ gadata <- google_analytics(viewId = 1234567,     # view ID changed
                             ), 
                            metrics = c("sessions"),
                            dimensions = c("date"),
-                           max = 6000)
+                           max = 5000)
 
 # line chart
 ggplot(gadata,aes(x=date,y=sessions,group=1)) +
